0ba3a128ccf43510fc2b539f0678f14d0daff8c5
dev-rj0t4 <renedesouza.jr@gmail.com>
2024-11-30 18:38:57 -0300

    first commit

--- pra.py
+++ pra.py
@@ -1,4 +1,3 @@
-velocidade = input('Informe sua velocidade')
-
+velocidade = int(input('Informe sua velocidade'))
 if (velocidade > 80):
     print("Você foi multado, valor 20 reais a cada km que exceder os 80km.")
